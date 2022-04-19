b48be0e66ab178d87efb0bb212d38ccc7c5c977e
juaniquinterosmood <jquinteros@moodtechnology.com.ar>
2022-04-19 17:46:29 -0300

    Hola marcos cree el update y solo hice un commit porque me la re banco

--- src/routes/property/index.js
+++ src/routes/property/index.js
@@ -1,6 +1,8 @@
 const nameModule = 'property/';
 const { app: create } = require('./src/create');
+const { app: update } = require('./src/update');
 
 module.exports = define => {
 	define(nameModule + 'create', create);
+	define(nameModule + 'update', update);
 };
---
+++ src/routes/property/src/update.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express.Router();
+const OwnerModel = require('../../../models/Owner');
+const InmoModel = require('../../../models/Inmo');
+const PropertyModel = require('../../../models/Property');
+const schemaProperty = require('../../../structures/property/update');
+const { messageForUserNotFound } = require('../../../messages/property/update');
+
+const handler = async (req, res) => {
+
+	const validation = await schemaProperty(req.body);
+
+	if(validation.error)
+		return res.status(400).json(validation);
+
+	try {
+		const userType = req.body.owner.type === 'owners' ?
+			await OwnerModel.getById(req.body.owner.id) :
+			await InmoModel.getById(req.body.owner.id);
+
+		if(!userType)
+			return res.status(404).json({ message: messageForUserNotFound });
+
+		const {
+			latitude, longitude, address, neighborhood, city, province, operation, type, title, description, rooms, price, images
+		} = req.body;
+
+		const dataProperty = {
+			...latitude && { latitude },
+			...longitude && { longitude },
+			...address && { address },
+			...neighborhood && { neighborhood },
+			...city && { city },
+			...province && { province },
+			owner: userType,
+			...operation && { operation },
+			...type && { type },
+			...title && { title },
+			...description && { description },
+			...rooms && { rooms },
+			...price && { price },
+			...images && { images }
+		};
+
+		const propertyInserted = await PropertyModel.update(req.params.id, dataProperty);
+
+		return res.status(200).json(propertyInserted);
+
+	} catch(error) {
+		return res.status(500).json(error.toString());
+	}
+};
+
+app.put('/:id', handler);
+
+module.exports = { app, handler };
